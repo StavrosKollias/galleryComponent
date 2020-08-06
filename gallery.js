e7c8f52d2d6ccf4a30b2c698efc20ba3f3a7e73d
Stavros Kollias <kollias1252@gmail.com>
2020-08-06 17:00:32 +0100

    not working properly grid ratio i will review it

--- gallery.js
+++ gallery.js
@@ -5,26 +5,62 @@ function getElements(type) {
 
 function loadGallery() {
   const images = getElements(".gallery-image");
-  const galleryListContainer = images[0].parentElement.parentElement;
-  const imageContainer = images[0].parentElement;
-  var gridRationWidth =
-    galleryListContainer.clientWidth / imageContainer.clientWidth;
-  gridRationWidth = Math.round(gridRationWidth);
-
-  galleryListContainer.style.gridTemplateColumns = `repeat(${gridRationWidth}, 1fr)`;
   const imagesLength = images.length;
 
-  images.forEach((e) => {
+  const galleryListContainer = images[0].parentElement.parentElement;
+  const imageContainer = images[0].parentElement;
+  const gridRatioWidth = findGridRatioWidth(
+    galleryListContainer,
+    imageContainer,
+    imagesLength
+  );
+  galleryListContainer.style.gridTemplateColumns = `repeat(${gridRatioWidth}, max-content)`;
+
+  checkRows(gridRatioWidth, imagesLength);
+  forEachImage(images);
+}
+
+function checkRows(gridRatioWidth, length) {
+  const countRows = 0;
+  const module = length % gridRatioWidth;
+  module > 0
+    ? console.log(module + " " + gridRatioWidth)
+    : console.log(module + " " + gridRatioWidth);
+}
+
+function forEachImage(listImages) {
+  listImages.forEach((e) => {
     var realWidth = e.naturalWidth;
     var realHeight = e.naturalHeight;
     var change = realWidth / e.parentElement.clientWidth;
     var newHeight = realHeight / change;
-    console.log(realWidth + " " + realHeight);
     e.style.width = "100%";
     e.parentElement.style.height = newHeight + "px";
   });
 }
 
+function findGridRatioWidth(
+  galleryListContainer,
+  imageContainer,
+  imagesLength
+) {
+  var gridRatioWidth =
+    galleryListContainer.clientWidth /
+    (imageContainer.clientWidth +
+      (galleryListContainer.clientWidth * 20) / imageContainer.clientWidth);
+  gridRatioWidth = Math.round(gridRatioWidth);
+
+  gridRatioWidth > imagesLength
+    ? (gridRatioWidth = imagesLength)
+    : gridRatioWidth;
+
+  return gridRatioWidth;
+}
+
 window.addEventListener("load", () => {
+  loadGallery();
+});
+
+window.addEventListener("resize", () => {
   loadGallery();
 });
