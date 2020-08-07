32c5e2be1f746b99c1b0f849ca1549ee5ec25f7a
Stavros Kollias <kollias1252@gmail.com>
2020-08-07 10:00:35 +0100

    re arrange

--- gallery.js
+++ gallery.js
@@ -1,30 +1,4 @@
-var resizeImagesContainer = function () {
-  var heightGridRows = getValCssPropertyElement(gallery, "grid-auto-rows");
-  var gapGrid = getValCssPropertyElement(gallery, "grid-row-gap");
-  gallery.querySelectorAll(".image-container").forEach(function (item) {
-    var el = item;
-    el.style.gridRowEnd =
-      "span " +
-      Math.ceil(
-        (getHeightContent(item) + gapGrid) / (heightGridRows + gapGrid)
-      );
-  });
-};
-
-const gallery = getElements(".gallery-images-container");
-
-gallery.querySelectorAll("img").forEach(function (item) {
-  var heightGridRows = getValCssPropertyElement(gallery, "grid-auto-rows");
-  var gapGrid = getValCssPropertyElement(gallery, "grid-row-gap");
-  var gitem = item.parentElement.parentElement;
-  gitem.style.gridRowEnd =
-    "span " +
-    Math.ceil((getHeightContent(gitem) + gapGrid) / (heightGridRows + gapGrid));
-});
-
-window.addEventListener("resize", resizeImagesContainer);
-
-//  ------ Functions ---------- Functions ----- Functions ------//
+//  ------ functions ---------- Functions ----- unctions ------
 
 var getValCssPropertyElement = function (elem, style) {
   return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
@@ -38,3 +12,29 @@ function getElements(type) {
   const elements = document.querySelectorAll(type);
   return elements;
 }
+
+var resizeImagesContainer = function () {
+  var heightGridRows = getValCssPropertyElement(gallery, "grid-auto-rows");
+  var gapGrid = getValCssPropertyElement(gallery, "grid-row-gap");
+  gallery.querySelectorAll(".image-container").forEach(function (item) {
+    var el = item;
+    el.style.gridRowEnd =
+      "span " +
+      Math.ceil(
+        (getHeightContent(item) + gapGrid) / (heightGridRows + gapGrid)
+      );
+  });
+};
+
+const gallery = document.querySelector(".gallery-images-container");
+
+gallery.querySelectorAll("img").forEach(function (item) {
+  var heightGridRows = getValCssPropertyElement(gallery, "grid-auto-rows");
+  var gapGrid = getValCssPropertyElement(gallery, "grid-row-gap");
+  var gitem = item.parentElement.parentElement;
+  gitem.style.gridRowEnd =
+    "span " +
+    Math.ceil((getHeightContent(gitem) + gapGrid) / (heightGridRows + gapGrid));
+});
+
+window.addEventListener("resize", resizeImagesContainer);
