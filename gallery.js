a37bd83686d0d34fd09bbba26bed841545174b33
Stavros Kollias <kollias1252@gmail.com>
2020-08-07 09:58:44 +0100

    Resizable dynam grid example

--- gallery.js
+++ gallery.js
@@ -1,14 +1,40 @@
+var resizeImagesContainer = function () {
+  var heightGridRows = getValCssPropertyElement(gallery, "grid-auto-rows");
+  var gapGrid = getValCssPropertyElement(gallery, "grid-row-gap");
+  gallery.querySelectorAll(".image-container").forEach(function (item) {
+    var el = item;
+    el.style.gridRowEnd =
+      "span " +
+      Math.ceil(
+        (getHeightContent(item) + gapGrid) / (heightGridRows + gapGrid)
+      );
+  });
+};
+
+const gallery = getElements(".gallery-images-container");
+
+gallery.querySelectorAll("img").forEach(function (item) {
+  var heightGridRows = getValCssPropertyElement(gallery, "grid-auto-rows");
+  var gapGrid = getValCssPropertyElement(gallery, "grid-row-gap");
+  var gitem = item.parentElement.parentElement;
+  gitem.style.gridRowEnd =
+    "span " +
+    Math.ceil((getHeightContent(gitem) + gapGrid) / (heightGridRows + gapGrid));
+});
+
+window.addEventListener("resize", resizeImagesContainer);
+
+//  ------ Functions ---------- Functions ----- Functions ------//
+
+var getValCssPropertyElement = function (elem, style) {
+  return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+};
+
+var getHeightContent = function (item) {
+  return item.querySelector(".content").getBoundingClientRect().height;
+};
+
 function getElements(type) {
   const elements = document.querySelectorAll(type);
   return elements;
 }
-
-function loadGallery() {}
-
-window.addEventListener("load", () => {
-  loadGallery();
-});
-
-window.addEventListener("resize", () => {
-  loadGallery();
-});
